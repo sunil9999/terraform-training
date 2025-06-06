24acf9938f08df1d26230d4797001b116011c8f9
sunil <35529134+sunil9999@users.noreply.github.com>
2025-06-06 17:39:50 +1000

    Add files via upload

---
+++ dev_backend.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "Sunil"
+storage_account_name = "testing12349876"
+container_name       = "test"
+key                  = "dev.terraform.tfstate"
---
+++ providers.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "dcc2ff58-62d2-4f1a-beab-e93129997ccc"
+}
+
+terraform {
+  backend "azurerm" {
+    
+  }
+}
